d966f1d9a19c4cc7365361b71578f5ca08963b71
wataruapacies <119409396+wataruapacies@users.noreply.github.com>
2023-01-28 14:40:00 +0000

    BOOKERS2-FIRST

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,56 @@
+class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update, :destroy]
+
+  def index
+    @list = Book.new
+    @lists= Book.all
+  end
+
+  def create
+    @list = Book.new(book_params)
+    @list.user_id = current_user.id
+    if @list.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@list.id)
+    else
+      @lists= Book.all
+      render :index
+    end
+  end
+
+  def show
+    @list = Book.find(params[:id])
+  end
+
+  def edit
+    @list = Book.find(params[:id])
+  end
+
+  def update
+    @list = Book.find(params[:id])
+    if @list.update(book_params)
+      flash[:notice_update] = "Book was successfully updated."
+      redirect_to book_path(@list.id)#"/books/#{list.id}"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @listroy = Book.find(params[:id])
+    @listroy.destroy
+    flash[:notice_destroy] = "Book was successfully destroyed."
+    redirect_to books_path#'/books'
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+  def is_matching_login_user
+    unless Book.find(params[:id]).user.id.to_i == current_user.id
+      redirect_to books_path
+    end
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  validates :email, uniqueness: true
+  validates :name, uniqueness: true
+  
+  validates :name,
+    length: { minimum: 2, maximum: 20 }
+  
+  validates :introduction,
+    length: { maximum: 50 }
+  
+  has_one_attached :profile_image
+  has_many :books, dependent: :destroy
+  
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+end
