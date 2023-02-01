ae47882e232824938560cb857c15fc5017b69660
wataruapacies <119409396+wataruapacies@users.noreply.github.com>
2023-02-01 09:08:01 +0000

    favorite-validation-error-OK

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,12 +2,19 @@ class BookCommentsController < ApplicationController
   before_action :is_matching_login_user, only: [:destroy]
 
   def create
-    @book = Book.find(params[:book_id])
-    @comment = BookComment.new(book_comment_params)
-    @comment.user_id = current_user.id
-    @comment.book_id = @book.id
-    @comment.save
-    redirect_to book_path(@book)
+    book = Book.find(params[:book_id])
+    comment = BookComment.new(book_comment_params)
+    comment.user_id = current_user.id
+    comment.book_id = book.id
+    if comment.save
+      redirect_to book_path(book)
+    else
+      @error_comment = comment
+      @list = Book.new
+      @book = Book.find(params[:book_id])
+      @book_comment = BookComment.new
+      render 'books/show'
+    end
   end
 
   def destroy
