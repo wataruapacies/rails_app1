d0318b8e0819a330d6c9bacc1fcb2c2d8cccf6e6
wataruapacies <119409396+wataruapacies@users.noreply.github.com>
2023-02-01 06:58:16 +0000

    comment-delete

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,31 @@
+class BookCommentsController < ApplicationController
+  before_action :is_matching_login_user, only: [:destroy]
+
+  def create
+    @book = Book.find(params[:book_id])
+    @comment = BookComment.new(book_comment_params)
+    @comment.user_id = current_user.id
+    @comment.book_id = @book.id
+    @comment.save
+    redirect_to book_path(@book)
+  end
+
+  def destroy
+    #@book = Book.find(params[:book_id])
+    BookComment.find(params[:id]).destroy
+    flash[:notice_comment_destroy] = "Comment was successfully destroyed."
+    redirect_to  book_path(params[:book_id])
+  end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+
+  def is_matching_login_user
+    unless BookComment.find(params[:id]).user.id.to_i == current_user.id
+      redirect_to books_path
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   get "home/about" => "homes#about", as:"about"
-  resources :books
+  resources :books do
+    resources :book_comments, only: [:create, :destroy]
+  end
   resources :users
 end
